a22a0b62d21ec958ad6713c1dbe79c734a230dc9
David Wood <devel@dtwood.uk>
2017-01-21 09:05:52 +0000

    pep8-ify

--- parse.py
+++ parse.py
@@ -10,51 +10,51 @@ def get_grammar():
     record_type = Enum(Byte, test=0x01, end=0xaa)
 
     test_result = Struct(
-        start = Const(b'\xfd'),
-        name = String(16),
-        units = String(16),
-        value = Int16ub,
-        flag = Flag,
+        start=Const(b'\xfd'),
+        name=String(16),
+        units=String(16),
+        value=Int16ub,
+        flag=Flag,
     )
     test_record = Struct(
-        success = pass_fail,
-        id_ = String(16),
-        zeros = Const(b'\x00')[64],
-        venue = String(16),
-        location = String(16),
-        unknown1 = String(5),
-        seven = Const(b'\xe0\x07'),
-        user = String(16),
-        comments = String(128),
-        unknown2 = String(1),
-        period1 = Int8ul,
-        test_type = String(30),
-        period2 = Int8ul,
-        unknown3 = String(15),
-        unknown4 = RawCopy(PascalString(Int8ul)),
-        start_results = Const(b'\xfe'),
-        results = RawCopy(test_result[:]),
-        unknown5 = Byte[this._.length - this.unknown4.length - this.results.length - 314],
+        success=pass_fail,
+        id_=String(16),
+        zeros=Const(b'\x00')[64],
+        venue=String(16),
+        location=String(16),
+        unknown1=String(5),
+        seven=Const(b'\xe0\x07'),
+        user=String(16),
+        comments=String(128),
+        unknown2=String(1),
+        period1=Int8ul,
+        test_type=String(30),
+        period2=Int8ul,
+        unknown3=String(15),
+        unknown4=RawCopy(PascalString(Int8ul)),
+        start_results=Const(b'\xfe'),
+        results=RawCopy(test_result[:]),
+        unknown5=Byte[this._.length - this.unknown4.length - this.results.length - 314],
     )
     final_record = Struct(
     )
-    record = Struct (
-        start = Const(b'\xff\x54'),
-        length = Int16ul,
-        checksum = Int16ul,
-        zeros = Const(b'\x00\x00'),
-        record_type = record_type,
-        data = Embedded(Switch(this.record_type, {
+    record = Struct(
+        start=Const(b'\xff\x54'),
+        length=Int16ul,
+        checksum=Int16ul,
+        zeros=Const(b'\x00\x00'),
+        record_type=record_type,
+        data=Embedded(Switch(this.record_type, {
             'test': test_record,
             'end': final_record,
         })),
     )
     pat_file = Struct(
-        unknown1 = Const(b'\x54\x2a\x00\x28\x06\x00\x00\x55'),
-        machine = String(20),
-        serial = String(20),
-        records = record[:],
-        eof = Const(b'\xff'),
+        unknown1=Const(b'\x54\x2a\x00\x28\x06\x00\x00\x55'),
+        machine=String(20),
+        serial=String(20),
+        records=record[:],
+        eof=Const(b'\xff'),
     )
 
     return pat_file

da2eb37146fc203747d4f78a491dd1bcf136d742
David Wood <devel@dtwood.uk>
2017-02-16 05:41:39 +0000

    Read direct from .gar file

---
+++ gar.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Paul Sladen, 2014-12-04, Seaward GAR PAT testing file format debug harness
+# Hereby placed in the public domain in the hopes of improving
+# electrical safety and interoperability.
+#
+# Example: gar.py <input1.gar> [input2.gar] ...
+#
+# This would extract the results to 'input1_TestResults.sss', and any
+# other JPEG attachments to 'input1_...jpg'.  Note that the JPEGs are
+# low-resolution (320x240---at least the ones I've seen); this is not
+# a problem with with extraction!
+
+# == GAR ==
+# GAR files are a container format used for importing/exporting
+# filesets to and from some Seaward PAT testing machines ("Apollo"
+# series?).  As of 2014-12-05 I have seen three examples of '.GAR'
+# files; one purporting to contain an .SSS file and a selection of
+# JPEGs and two purporting to contain just an .SSS file.
+#
+# == File Header ==
+# There is a single file-header that begins 0xcabcab (CAB CAB;
+# cabinet?) for magic/identification purposes, followed by single byte
+# version number.  There is no-end of file marker or overall checksum.
+#
+# == Archive records ==
+# Each file stored within the GAR container is prefixed by:
+# 1. a record header giving the overall size of the record;
+# 2. a variable length human-readable string (aka the filename);
+# 3. a truncated monotonically-increasing semi-timestamp;
+# 4. the original pre-compression length.
+#
+# == Compression ==
+# Compression is Deflate---as used in zipfiles and PNG images---
+# wrapped with zlib headers and footers.  This reduces the largely
+# fixed-field .SSS files to ~10% of their input size, while the
+# already-compressed JPEG files remain ~99% of their input size.
+# Each file's payload is compressed separately.
+#
+# == Deflate ==
+# The QByteArray::qCompress() convention is used, which prepends an
+# extra four-byte header containing an additional little-endian
+# uncompressed stream size, then continues with the usual two-byte
+# zlib header, deflate streame, and four-byte zlib footer:
+# http://ehc.ac/p/ctypes/mailman/message/23484411/
+#
+# As the contained files are likely to be under 65k in length, the
+# first 2 bytes of the original length are nulls, which proved to be a
+# handy plaintext and sanity check for the next step. :-D
+#
+# There appear to be some people at Seaward posting patches to Qt
+# so I expect the added qCompress() length prefix is probably simply
+# as a result of using Qt somewhere, rather than an active choice:
+# http://patchwork.openembedded.org/patch/80259/mbox/
+#
+# == Obfuscation ==
+# The qCompress (with length prefix)-style Deflate/zlib streams
+# are perturbed additively (meaning bytewise ADD/SUB, not XOR) with
+# the bottom 8-bits from Marsaglia xorshift PNR, seeded from the
+# pseudo-timestamp and payload length of the corresponding file.
+#
+# Xorshift uses 128-bits (four * 32-bit words) for state (x,y,z,w),
+# and the standard default seeds are used for initialisation for
+# 'z' and 'w' while, 'x' and 'y' are seeded from truncated timestamp
+# and from the original file-length.  The give-away for spotting
+# xorshift is that the first round output depends solely upon the
+# 'x' and 'w' inputs, enabling confirmation of 'x' (== timestamp)
+# independent to confirmation of 'y' (== original file length).
+#
+# 5. original length (little-endian, four octets)
+# 6. compressed contents (zlib)
+#
+# == Integrity checking ==
+# Aside from failing to decode/validate, the zlib checksum provides
+# the only defacto integrity checking in the GAR file-format.
+
+import io
+import struct
+import sys
+import zlib
+
+
+# Marsaglia xorshift, using default parameters
+# https://en.wikipedia.org/wiki/Xorshift
+# http://stackoverflow.com/questions/4508043/on-xorshift-random-number-generator-algorithm
+def marsaglia_xorshift_128(x=123456789, y=362436069, z=521288629, w=88675123):
+    while True:
+        t = (x ^ (x << 11)) & 0xffffffff
+        x, y, z = y, z, w
+        w = (w ^ (w >> 19) ^ (t ^ (t >> 8)))
+        yield w
+
+
+# The lower 8-bits from the Xorshift PNR are subtracted from byte
+# values during extraction, and added to byte values on insertion.
+# When calling deobfuscate_string() the whole string is processed.
+def deobfuscate_string(pnr, obfuscated, operation=int.__sub__):
+    return b''.join(
+        bytes([operation(c, next(pnr)) & 0xff])
+        for c in obfuscated
+    )
+
+
+# Remove spaces and directory slashes from a string (filename).
+# This is useful for saving a file in the current directory, rather
+# than needing to recreate the structure of the container.
+def clean_filename(unsafe_filename):
+    return unsafe_filename \
+            .replace('/', '_') \
+            .replace(' ', '_') \
+            .replace('\\', '_')
+
+
+# The main parse and extract from Seaward '.GAR' container starts here
+def get_gar_contents(container):
+    if not hasattr(container, 'read'):
+        container = io.BytesIO(container)
+    # The GAR container's magic number is 0xcabcab
+    container_header = struct.unpack('>L', container.read(4))[0]
+    container_magic = container_header >> 8
+    assert(container_magic == 0xcabcab)
+    container_version = container_header & 0xff
+    assert(container_version == 1)
+
+    output = {}
+
+    # The container has no end-of-file marker,
+    # it ends when there are no more records
+    while True:
+        s = container.read(4)
+        if len(s) < 4:
+            break
+
+        # The record headers start with a variable length (filename) string
+        filename_length, = struct.unpack('>L', s)
+        filename = container.read(filename_length).decode('utf-8')
+
+        # Followed by a file contents, variable length depending on compression
+        compressed_length, = struct.unpack('>L', container.read(4))
+        contents = container.read(compressed_length)
+        header_length, mangling_method, truncated_timestamp, original_length = struct.unpack('>HHLL', contents[:12])
+        assert(header_length == 12)
+        assert(mangling_method == 1)
+
+        # The file contents are obfuscated with a Marsaglia xorshift PNR
+        pnr = marsaglia_xorshift_128(x=truncated_timestamp, y=original_length)
+
+        # There is also a (second) obfuscated copy of the original file length
+        # and then the (compressed) file contents.
+        qcompress_prefix = deobfuscate_string(pnr, contents[12:16])
+        zlib_stream = deobfuscate_string(pnr, contents[16:])
+
+        # We can check the lengths match up, and if so try to uncompress with zlib
+        expected_length, = struct.unpack(">L", qcompress_prefix)
+        assert(original_length == expected_length)
+        original = zlib.decompress(zlib_stream)
+
+        # Assuming it all went well we can inform the user where their file will be saved
+        assert(original_length == expected_length == len(original))
+
+        # And then write out the original uncompressed file to its appropriate name
+        output[filename] = original
+
+    return output
--- parse.py
+++ parse.py
@@ -1,204 +1,223 @@
 #! /usr/bin/env python3
 
 from construct import *
+import datetime
 import sys
+import gar
+
+
+def trace(x):
+    print(x)
+    return x
 
 
 class CustomFloat16(Adapter):
     def __init__(self, units, *args, **kwargs):
         self.units = units
-        super().__init__(Bytes(2), *args, **kwargs)
+        super().__init__(Int16ul, *args, **kwargs)
 
     def _decode(self, obj, context):
         return {
-            'value': (obj[0] & 0x3fff) * (0.1 ** ((obj[1] & 0xc000) >> 14)),
+            'value': (obj & 0x3fff) * (0.1 ** ((obj & 0xc000) >> 14)),
             'units': self.units,
         }
 
 
-def get_grammar():
-    pass_fail = Enum(Byte, pass_=1, fail=2)
-    record_type = Enum(Byte, test=0x01, end=0xaa, machine_info=0x55)
+pass_fail = Enum(Byte, pass_=1, fail=2)
+record_type = Enum(Byte, test=0x01, end=0xaa, machine_info=0x55)
+physical_test_type = Enum(
+    Byte,
+    earth_resistance=0x11,
+    iec=0x16,
+    insulation=0x20,
+    polarity=0x91,
+    mains_voltage=0x92,
+    earth_leakage=0x96,
+    rcd=0x9a,
+    substitute_leakage=0x83,
+    string=0xfd,
+)
 
-    mains_voltage = Struct(
-        voltage=CustomFloat16('ohm'),
-        result=Byte,
-    )
-    earth_leakage = Struct(
-        unknown1=CustomFloat16('???'),
-        unknown2=CustomFloat16('???'),
-        unknown3=CustomFloat16('???'),
-        result=Byte,
-    )
-    rcd = Struct(
-        unknown1=CustomFloat16('???'),
-        unknown2=CustomFloat16('???'),
-        unknown3=CustomFloat16('???'),
-        result=Byte,
-    )
-    earth_resistance = Struct(
-        resistance=CustomFloat16('ohm'),
-        result=Byte,
-    )
-    iec = Struct(
-        resistance=CustomFloat16('ohm'),
-        result=Byte,
-    )
-    insulation = Struct(
-        voltage=CustomFloat16('volt'),
-        resistance=CustomFloat16('megaohm'),
-        result=Byte,
-    )
-    polarity = Struct(
-        result=Byte,
-    )
-    substitute_leakage = Struct(
-        unknown=CustomFloat16('???'),
-        result=Byte,
-    )
-    string = Struct(
-        value=String(34),
-        result=Byte,
-    )
-    physical_test_type = Enum(
-        Byte,
-        earth_resistance=0x11,
-        iec=0x16,
-        insulation=0x20,
-        polarity=0x91,
-        mains_voltage=0x92,
-        earth_leakage=0x96,
-        rcd=0x9a,
-        substitute_leakage=0x83,
-        string=0xfd,
-    )
-    physical_test_result = Struct(
-        ty=physical_test_type,
-        value=Switch(
-            this.ty,
-            {
-                'earth_resistance': earth_resistance,
-                'iec': iec,
-                'insulation': insulation,
-                'polarity': polarity,
-                'mains_voltage': mains_voltage,
-                'earth_leakage': earth_leakage,
-                'rcd': rcd,
-                'substitute_leakage': substitute_leakage,
-                'string': string,
-            }
-        ),
-    )
+mains_voltage = Struct(
+    voltage=CustomFloat16('ohm'),
+    result=Byte,
+)
+earth_leakage = Struct(
+    unknown1=CustomFloat16('???'),
+    unknown2=CustomFloat16('???'),
+    unknown3=CustomFloat16('???'),
+    result=Byte,
+)
+rcd = Struct(
+    unknown1=CustomFloat16('???'),
+    unknown2=CustomFloat16('???'),
+    unknown3=CustomFloat16('???'),
+    result=Byte,
+)
+earth_resistance = Struct(
+    resistance=CustomFloat16('ohm'),
+    result=Byte,
+)
+iec = Struct(
+    resistance=CustomFloat16('ohm'),
+    result=Byte,
+)
+insulation = Struct(
+    voltage=CustomFloat16('volt'),
+    resistance=CustomFloat16('megaohm'),
+    result=Byte,
+)
+polarity = Struct(
+    result=Byte,
+)
+substitute_leakage = Struct(
+    current=CustomFloat16('milliamp'),
+    result=Byte,
+)
+string = Struct(
+    value=String(34),
+    result=Byte,
+)
+physical_test_result = Struct(
+    ty=physical_test_type,
+    value=Switch(
+        this.ty,
+        {
+            'earth_resistance': earth_resistance,
+            'iec': iec,
+            'insulation': insulation,
+            'polarity': polarity,
+            'mains_voltage': mains_voltage,
+            'earth_leakage': earth_leakage,
+            'rcd': rcd,
+            'substitute_leakage': substitute_leakage,
+            'string': string,
+        }
+    ),
+)
+visual_test_result = Struct(
+    start=Const(b'\xfd'),
+    name=String(16),
+    units=String(16),
+    value=Int16ul,
+    flag=Flag,
+)
 
-    machine_info_record = Struct(
-        machine=String(20),
-        serial=String(20),
-    )
-    visual_test_result = Struct(
-        start=Const(b'\xfd'),
-        name=String(16),
-        units=String(16),
-        value=Int16ul,
-        flag=Flag,
-    )
-    test_record = Struct(
-        success=pass_fail,
-        id_=String(16),
-        zeros=Const(b'\x00')[64],
-        venue=String(16),
-        location=String(16),
-        hour=Int8ul,
-        minute=Int8ul,
-        second=Int8ul,
-        day=Int8ul,
-        month=Int8ul,
-        year=Int16ul,
-        user=String(16),
-        comments=String(128),
-        unknown1=Const(b'\x02'),
-        full_retest_period=Int8ul,
-        test_type=String(30),
-        visual_retest_period=Int8ul,
-        unknown2=String(15),
-        unknown3=PascalString(Int8ul),
-        start_results=Const(b'\xfe'),
-        visual_test_results=visual_test_result[:],
-        physical_test_results=physical_test_result[:],
-    )
-    final_record = Struct(
-    )
-    record = Struct(
-        start=Const(b'\x54'),
-        length=Int16ul,
-        checksum=Int16ul,
-        zeros=Const(b'\x00\x00'),
-        record_type=RawCopy(record_type),
-        data=RawCopy(Switch(this.record_type.value, {
-            'machine_info': machine_info_record,
-            'test': test_record,
-            'end': final_record,
-        })),
-        end=Const(b'\xff'),
-        checksum_computed=Computed(sum_(this.record_type.data + this.data.data + this.end) & 0xffff)
-    )
-    pat_file = Struct(
-        records=record[:],
-    )
-
-    return pat_file
+machine_info_record = Struct(
+    machine=String(20),
+    serial=String(20),
+)
+test_record = Struct(
+    success=pass_fail,
+    id_=String(16),
+    zeros=Const(b'\x00')[64],
+    venue=String(16),
+    location=String(16),
+    hour=Int8ul,
+    minute=Int8ul,
+    second=Int8ul,
+    day=Int8ul,
+    month=Int8ul,
+    year=Int16ul,
+    user=String(16),
+    comments=String(128),
+    unknown1=Const(b'\x02'),
+    full_retest_period=Int8ul,
+    test_type=String(30),
+    visual_retest_period=Int8ul,
+    unknown2=String(15),
+    unknown3=PascalString(Int8ul),
+    start_results=Const(b'\xfe'),
+    visual_test_results=visual_test_result[:],
+    physical_test_results=physical_test_result[:],
+)
+final_record = Struct(
+)
+record = Struct(
+    start=Const(b'\x54'),
+    length=Int16ul,
+    checksum=Int16ul,
+    zeros=Const(b'\x00\x00'),
+    record_type=RawCopy(record_type),
+    data=RawCopy(Switch(this.record_type.value, {
+        'machine_info': machine_info_record,
+        'test': test_record,
+        'end': final_record,
+    })),
+    end=Const(b'\xff'),
+    checksum_computed=Computed(sum_(
+        this.record_type.data + this.data.data + this.end) &
+                               0xffff)
+)
+pat_file = Struct(
+    records=record[:],
+)
 
 
-def main():
-    grammar = get_grammar()
-
-    with open(sys.argv[1], 'rb') as f:
-        result = grammar.parse(f.read())
+def get_results(data):
+    extracted_gar = gar.get_gar_contents(data)
+    result = pat_file.parse(extracted_gar['TestResults.sss'])
+    del extracted_gar['TestResults.sss']
 
     assert(result.records[-1].record_type.value == 'end')
     for record in result.records:
         assert(record.checksum == record.checksum_computed or
                record.checksum == record.checksum_computed - 1)
 
+    return {
+        'results': [
+            {
+                'id': record.data.value.id_.decode("utf-8"),
+                'venue': record.data.value.venue.decode("utf-8"),
+                'location': record.data.value.location.decode("utf-8"),
+                'visual_retest_period':
+                    datetime.timedelta(days=record.data.value.visual_retest_period * 30),
+                'full_retest_period':
+                    datetime.timedelta(days=record.data.value.full_retest_period * 30),
+                'test_time':  datetime.datetime(
+                    year=record.data.value.year,
+                    month=record.data.value.month,
+                    day=record.data.value.day,
+                    hour=record.data.value.hour,
+                    minute=record.data.value.minute,
+                    second=record.data.value.second,
+                ),
+                'test_type': record.data.value.test_type.decode('utf-8'),
+                'comments': record.data.value.comments.decode('utf-8'),
+                'subtests': {
+                    'visual': {
+                        'result':
+                            'fail' if record.data.value.success == 'fail' and
+                            len(record.data.value.physical_test_results) == 0
+                            else 'pass',
+                    }, **{
+                        result.ty: dict(result.value)
+                        for result in record.data.value.physical_test_results
+                    },
+                },
+                'result': 'pass' if record.data.value.success == 'pass_' else 'fail',
+            }
+            for record in result.records
+            if record.record_type.value == 'test'
+        ],
+        'images': extracted_gar,
+    }
+
+
+def main():
+    try:
+        f = sys.argv[1]
+    except IndexError:
+        f = "ApolloDownload.gar"
+
+    with open(f, 'rb') as f:
+        output = get_results(f.read())
+
+    output['results'].sort(key=lambda result: result['test_time'])
+
     import pprint
-    import datetime
-
-    output = [
-        {
-            'id': record.data.value.id_.decode("utf-8"),
-            'venue': record.data.value.venue.decode("utf-8"),
-            'location': record.data.value.location.decode("utf-8"),
-            'visual_retest_period':
-                datetime.timedelta(days=record.data.value.visual_retest_period*30),
-            'full_retest_period':
-                datetime.timedelta(days=record.data.value.full_retest_period*30),
-            'test_time':  datetime.datetime(
-                year=record.data.value.year,
-                month=record.data.value.month,
-                day=record.data.value.day,
-                hour=record.data.value.hour,
-                minute=record.data.value.minute,
-                second=record.data.value.second,
-            ),
-            'test_type': record.data.value.test_type.decode('utf-8'),
-            'comments': record.data.value.comments.decode('utf-8'),
-            'subtests': {**{
-                'visual': {
-                    'result':
-                        'fail' if record.data.value.success == 'fail' and
-                        len(record.data.value.physical_test_results) == 0
-                        else 'pass',
-                }, **{
-                    result.ty: dict(result.value)
-                    for result in record.data.value.physical_test_results
-                }},
-            },
-            'result': 'pass' if record.data.value.success == 'pass_' else 'fail',
-        }
-        for record in result.records
-        if record.record_type.value == 'test'
-    ]
-
     pprint.pprint(output)
+
 
 if __name__ == '__main__':
     main()
